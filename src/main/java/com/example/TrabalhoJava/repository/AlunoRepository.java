32574ea4ddd69d86295f7889e2c6dcc29ff43efb
ElcioAngelo <AngeloNegri32@outlook.com>
2024-11-08 21:24:42 -0300

    Operações Crud para Criar, Ler, Atualizar e Remover Alunos

--- src/main/java/com/example/TrabalhoJava/repository/AlunoRepository.java
+++ src/main/java/com/example/TrabalhoJava/repository/AlunoRepository.java
@@ -4,6 +4,4 @@ import com.example.TrabalhoJava.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
-
-    Optional<Aluno> findByEmail(String email);
 }
