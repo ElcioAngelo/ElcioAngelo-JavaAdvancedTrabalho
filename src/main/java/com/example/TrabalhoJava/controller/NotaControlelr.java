074aa754a1d88d9704395ebdd9efba1ba4d2a712
ElcioAngelo <AngeloNegri32@outlook.com>
2024-11-26 15:38:25 -0300

    "Notas - Matriculas, arrumando JSON"

--- src/main/java/com/example/TrabalhoJava/controller/NotaControlelr.java
+++ src/main/java/com/example/TrabalhoJava/controller/NotaControlelr.java
@@ -1,28 +1,27 @@
 package com.example.TrabalhoJava.controller;
 
-import com.example.TrabalhoJava.model.Nota;
+//import com.example.TrabalhoJava.model.Nota;
 import com.example.TrabalhoJava.repository.NotaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+//import java.util.List;
 
 @RestController
 @RequestMapping("/api/notas")
 public class NotaControlelr {
 
-    @Autowired
-    private NotaRepository repository;
+    //@Autowired
+    //private NotaRepository repository;
 
-    @PostMapping
-    public ResponseEntity<List<Nota>> findAll(){
-        List<Nota> nota = this.repository.findAll();
-        if(nota.isEmpty()){
-
-        }
-    }
+//    @PostMapping
+//    public ResponseEntity<List<Nota>> findAll(){
+//        List<Nota> nota = this.repository.findAll();
+//        if(nota.isEmpty()){
+//
+//        }
+//    }
 }
